d920a6badf94e5afdd38bf00890eaa36b5ca439f
David Dvali <david.dvali@gmail.com>
2023-01-31 00:33:48 +0400

    Add DbContext DI to API and few startup stuff

--- TBC.Task.API/Program.cs
+++ TBC.Task.API/Program.cs
@@ -1,11 +1,15 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using TBC.Task.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PersonsDbContext>(options =>
+	options.UseSqlServer(configuration.GetConnectionString("TbcPersons")));
 
 var app = builder.Build();
 
@@ -13,6 +17,7 @@ if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
+	app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
