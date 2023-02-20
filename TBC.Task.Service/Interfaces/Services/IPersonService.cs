80cbce62457e5460f78dde9f0051b4a73e5ef1e1
David Dvali <david.dvali@gmail.com>
2023-02-21 01:44:29 +0400

    Fix PersonService list return types

--- TBC.Task.Service/Interfaces/Services/IPersonService.cs
+++ TBC.Task.Service/Interfaces/Services/IPersonService.cs
@@ -5,7 +5,7 @@ namespace TBC.Task.Service.Interfaces.Services;
 public interface IPersonService : IQueryService<Person>, ICommandService<Person>
 {
 	Person? GetIncludeCity(int id);
-	(IQueryable<Person>, int) QuickSearch(string keyword, int currentPage, int pageSize);
-	(IQueryable<Person>, int) Search(string keyword, DateTime? birthDateFrom, DateTime? birthDateTo, int currentPage, int pageSize);
+	(IEnumerable<Person>, int) QuickSearch(string keyword, int currentPage, int pageSize);
+	(IEnumerable<Person>, int) Search(string keyword, DateTime? birthDateFrom, DateTime? birthDateTo, int currentPage, int pageSize);
 	bool Exists(int id);
 }
